e493a5aec56f2179383b6eb3c8169492fc5fddc3
ashraysripathi <42443206+ashraysripathi@users.noreply.github.com>
2020-02-27 16:00:39 +0530

    test2
    another test just to confirm I havent missed anything

--- download/download_curves.py
+++ download/download_curves.py
@@ -1,1 +1,11 @@
-print("test")
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import csv
+import os
+import stat
+import sys
+
+print("Completed")
