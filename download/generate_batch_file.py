374eacc5528808b526ae02918425bdce70766c80
ashraysripathi <42443206+ashraysripathi@users.noreply.github.com>
2020-02-29 13:47:29 +0530

    redid download script
    i am stupid

--- download/generate_batch_file.py
+++ download/generate_batch_file.py
@@ -8,7 +8,7 @@ import os
 import stat
 import sys
 
-wget_cmd = ("wget -q -nH --cut-dirs=6 -r -l0 -c -N -np -erobots=off "
+wget_cmd = ("wget -q -nH --cut-dirs=6 -r -l0 -c -nc -np -erobots=off "
              "-R 'index*' -A _llc.fits")
 url_b="http://archive.stsci.edu/pub/kepler/lightcurves"
 dr24csv_file="D:/Final_Year_Project/dr24tce.csv"
@@ -22,18 +22,19 @@ with open(dr24csv_file) as f:
 
 num_kepids = len(kepler_ids)
 
-with open("download/download.bat","w") as f:
+
+with open("download/download2.bat","w") as f:
 
   f.write("ECHO 'Downloading {} Kepler targets to {}'\n".format(num_kepids, download_directory))
   for i, kepid in enumerate(kepler_ids):
     print("Next")
-    if i and not i % 10:
-      f.write("echo 'Downloaded {}/{}'\n".format(i, num_kepids))
-      kepid = "{0:09d}".format(int(kepid))
-      subdir = "{}/{}".format(kepid[0:4], kepid)
-      download_dir = os.path.join(download_directory, subdir)
-      url = "{}/{}/".format(url_b, subdir)
-      f.write("{} -P {} {}\n".format(wget_cmd, download_dir, url))
+    
+    f.write("echo 'Downloaded {}/{}'\n".format(i, num_kepids))
+    kepid = "{0:09d}".format(int(kepid))
+    subdir = "{}/{}".format(kepid[0:4], kepid)
+    download_dir = os.path.join(download_directory, subdir)
+    url = "{}/{}/".format(url_b, subdir)
+    f.write("{} -P {} {}\n".format(wget_cmd, download_dir, url))
   f.write("echo 'Finished downloading {} Kepler targets to {}'\n".format(num_kepids, download_directory))
 
 
